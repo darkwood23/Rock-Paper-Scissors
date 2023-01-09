3df36d03f5bc7314e2a72ce17b98f4a116bf4760
dark wood <anonymoushackerintheweb@gmail.com>
2023-01-09 11:09:58 +0545

    Edit script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
- 
+function getComputerChoice(min, max) {
+    let computerChoice =  Math.floor(Math.random() * (max - min + 1) + min);
+    if (computerChoice === 1) {
+        return "rock"
+    } else if (computerChoice === 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+let playerChoice = prompt("Enter your choce (rock, paper, scissors): ").toLowerCase()
+let computerChoice = getComputerChoice(1, 3)
+
+console.log("Your Choice:", playerChoice)
+console.log("Computer Choice:", computerChoice)
+
+if (playerChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! ")
+} else if (playerChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose!")
+} else if (playerChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose!")
+} else if (playerChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win!")
+} else if (playerChoice === "paper" && computerChoice === "rock") {
+    console.log("You win!")
+} else if (playerChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win!")
+} else if (playerChoice === computerChoice){
+    console.log("Draw!")
+} else {
+    console.log("Please choose one of the three options!")
+}
