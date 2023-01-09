5b14bd17f05cd1afb904fb90679f63d7b6a47e5a
dark wood <anonymoushackerintheweb@gmail.com>
2023-01-09 11:22:10 +0545

    Edit script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerPoints = 0
+let computerPoints = 0
+
 function getComputerChoice(min, max) {
     let computerChoice =  Math.floor(Math.random() * (max - min + 1) + min);
     if (computerChoice === 1) {
@@ -9,26 +12,50 @@ function getComputerChoice(min, max) {
     }
 }
 
-let playerChoice = prompt("Enter your choce (rock, paper, scissors): ").toLowerCase()
-let computerChoice = getComputerChoice(1, 3)
+function playGame() {
+    let playerChoice = prompt("Enter your choce (rock, paper, scissors): ").toLowerCase()
+    let computerChoice = getComputerChoice(1, 3)
 
-console.log("Your Choice:", playerChoice)
-console.log("Computer Choice:", computerChoice)
+    console.log("Your Choice:", playerChoice)
+    console.log("Computer Choice:", computerChoice)
 
-if (playerChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! ")
-} else if (playerChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose!")
-} else if (playerChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose!")
-} else if (playerChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win!")
-} else if (playerChoice === "paper" && computerChoice === "rock") {
-    console.log("You win!")
-} else if (playerChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win!")
-} else if (playerChoice === computerChoice){
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! ")
+        computerPoints += 1
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose!")
+        computerPoints += 1
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose!")
+        computerPoints += 1
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win!")
+        playerPoints += 1
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("You win!")
+        playerPoints += 1
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win!")
+        playerPoints += 1
+    } else if (playerChoice === computerChoice){
+        console.log("Draw!")
+    } else {
+        console.log("Please choose one of the three options!")
+    }
+}
+
+for (i = 1; i <= 5; i++) {
+    console.log("Round" + i)
+    playGame()
+}
+
+console.log("Computer Points:", computerPoints)
+console.log("Your Points:", playerPoints)
+
+if (computerPoints === playerPoints){
     console.log("Draw!")
+} else if (computerPoints > playerPoints) {
+    console.log("You lost the match!!")
 } else {
-    console.log("Please choose one of the three options!")
+    console.log("You won the match!!")
 }
