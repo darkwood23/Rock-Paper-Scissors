4712f113323a6f4bd74d5b6fc327c901f36f46ab
darkwood23 <anonymoushackerintheweb@gmail.com>
2023-01-18 18:12:39 +0545

    Working Game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerPoints = 0
 let computerPoints = 0
+const container = document.querySelector("#displayCode")
 
 function getComputerChoice(min, max) {
     let computerChoice =  Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,8 +13,8 @@ function getComputerChoice(min, max) {
     }
 }
 
-function playGame() {
-    let playerChoice = prompt("Enter your choce (rock, paper, scissors): ").toLowerCase()
+function playGame(btnType) {
+    let playerChoice = document.getElementById(btnType).textContent.toLowerCase()
     let computerChoice = getComputerChoice(1, 3)
 
     console.log("Your Choice:", playerChoice)
@@ -42,20 +43,66 @@ function playGame() {
     } else {
         console.log("Please choose one of the three options!")
     }
+
+    checkWinner()
+    displayScore()
 }
 
-for (i = 1; i <= 5; i++) {
-    console.log("Round" + i)
-    playGame()
+function checkWinner(){
+    if (computerPoints === playerPoints){
+        console.log("Draw!")
+    } else if (computerPoints > playerPoints) {
+        console.log("You lost the match!!")
+    } else {
+        console.log("You won the match!!")
+    }
 }
 
-console.log("Computer Points:", computerPoints)
-console.log("Your Points:", playerPoints)
+function displayScore() {
+    console.log("Computer Points:", computerPoints)
+    console.log("Your Points:", playerPoints)
+}
 
-if (computerPoints === playerPoints){
-    console.log("Draw!")
-} else if (computerPoints > playerPoints) {
-    console.log("You lost the match!!")
-} else {
-    console.log("You won the match!!")
+
+function displayPlay(btnType) {
+    let playerChoice = document.getElementById(btnType).textContent.toLowerCase()
+    let computerChoice = getComputerChoice(1, 3)
+
+    const pTag = document.createElement("p")
+    pTag.textContent = "Your Choice:" + playerChoice
+    container.appendChild(pTag)
+
+    const pTagTwo = document.createElement('p')
+    pTagTwo.textContent = "Computer choice:" + computerChoice
+    container.appendChild(pTagTwo)
+
+    const pTagThree = document.createElement('p')
+    pTagThree.textContent = checkChoice(playerChoice, computerChoice)
+    container.appendChild(pTagThree)
+}
+
+function checkChoice(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        return "You lose!"
+        computerPoints += 1
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        return "You lose!"
+        computerPoints += 1
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        return "You lose!"
+        computerPoints += 1
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "You win!"
+        playerPoints += 1
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "You win!"
+        playerPoints += 1
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        return "You win!"
+        playerPoints += 1
+    } else if (playerChoice === computerChoice){
+        return "Draw!"
+    } else {
+        return("Please choose one of the three options!")
+    }
 }
